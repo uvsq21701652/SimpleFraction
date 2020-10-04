692ef2162a3b5531faee363c28a41396fc8d2cfd
Sofiane Hamad <sofiane.hamad@ens.uvsq.fr>
2020-10-04 20:04:24 +0200

    commit 2

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
 
+public class Main {
+
+
+	public static void main(String[] args){
+
+		int n,d;
+
+		n = 4; d = 7;
+			
+		
+
+		System.out.println(" fraction  = " + n + " / " + d + "\n");
+
+
+	} 
+}
+
+
