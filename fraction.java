1b5f26e388a994a8aa0df3a2bf9f41a77fd16a55
Sofiane Hamad <sofiane.hamad@ens.uvsq.fr>
2020-10-04 18:57:04 +0200

    commit 07

--- fraction.java
+++ fraction.java
@@ -1,1 +1,24 @@
+import java.awt.*;
+import java.util.*;
+
+
+public class fraction{
+
+	private int a,b;
+
+	public fraction(int a,int b){
+
+		this.a = a;
+		this.b = b;
+		
+		
+	}
+
+
+	public String toString(){
+
+		return a + " / " + b;
+	}
+
+}
 
